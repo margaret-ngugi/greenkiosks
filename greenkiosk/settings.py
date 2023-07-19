ae343c4a1c5b8f38d46558a91c13366b5e93f31f
margaret ngugi <ngugiwairimumargaret@gmail.com>
2023-07-19 23:44:01 +0300

    changes

--- greenkiosk/settings.py
+++ greenkiosk/settings.py
@@ -10,10 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,11 +63,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'greenkiosk.urls'
+  
 
 TEMPLATES = [
+    
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+      
+        'DIRS':  [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import ProductUploadForm
+# from django.shortcuts import render
+
+def upload_product(request):
+    form = ProductUploadForm()
+    return render(request, 'inventory/product_upload.html', {'form': form})
 
 # Create your views here.
