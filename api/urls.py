5ba849223a58003de3fd16f582fbeb696c218df7
margaret ngugi <ngugiwairimumargaret@gmail.com>
2023-09-08 11:05:35 +0300

    changes

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import CustomerListView,CustomerDetailView
+from .views import CustomerListView,CustomerDetailView,ProductListView,CartListView,OrderListView,ProductDetailView,CartDetailView,OrderDetailView
+
+
+
 urlpatterns = [
     # path("customers/", CustomerListView.as_view(), name = (customer_list_view))
     path("customers/", CustomerListView.as_view(), name="customer_list_view"),
-    path("customers/<int:id>", CustomerDetailView.as_view(), name="customer_detail_view")
+    path("customers/<int:id>", CustomerDetailView.as_view(), name="customer_detail_view"),
+    path("products/<int:id>", ProductDetailView.as_view(), name="product_detail_view"),
+    path("products/", ProductListView.as_view(), name="product_list_view"),
+    path("carts/<int:id>", CartDetailView.as_view(), name="cart_detail_view"),
+    path("carts/", CartListView.as_view(), name="cart_list_view"),
+    path("orders/", OrderListView.as_view(), name="order_list_view"),
+    path("orders/<int:id>", OrderDetailView.as_view(), name="order_detail_view"),
+    
+  
+  
 ]
 
 
--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
-from .serializers import CustomerSerializer
+from .serializers import CustomerSerializer,ProductSerializer,CartSerializer,OrderSerializer
 from rest_framework.views import APIView
 from customer.models import Customer
+from inventory.models import Product
 from rest_framework.response import Response
 from rest_framework import status
+from cart.models import Cart
+from order.models import Order
+
+
 class CustomerListView(APIView):
     def get(self, request):
         customers = Customer.objects.all()
@@ -14,6 +19,7 @@ class CustomerListView(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
 class CustomerDetailView(APIView):
     def get(self,request,id,format=None):
         customer = Customer.objects.get(id=id)
@@ -30,3 +36,102 @@ class CustomerDetailView(APIView):
         customer = Customer.objects.get(id=id)
         customer.delete()
         return Response("customer deleted", status=status.HTTP_204_NO_CONTENT)
+    
+    
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer= ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)  
+    
+    
+class ProductDetailView(APIView):
+    def get(self,request,id,format=None):
+        product = Product.objects.get(id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    def put(self,request,id,format=None):
+        product = Product.objects.get(id=id)
+        serializer = ProductSerializer(product,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,id,format=None):
+        product = Product.objects.get(id=id)
+        product.delete()
+        return Response("product deleted", status=status.HTTP_204_NO_CONTENT)
+        
+    
+class CartListView(APIView):
+    def get(self, request):
+        carts = Cart.objects.all()
+        serializer = CartSerializer(carts, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer= CartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+class CartDetailView(APIView):
+    def get(self,request,id,format=None):
+        cart = Cart.objects.get(id=id)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+    def put(self,request,id,format=None):
+        cart = Cart.objects.get(id=id)
+        serializer = CartSerializer(cart,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,id,format=None):
+        cart = Cart.objects.get(id=id)
+        cart.delete()
+        return Response("cart deleted", status=status.HTTP_204_NO_CONTENT)
+            
+    
+class OrderListView(APIView):
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer= OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class OrderDetailView(APIView):
+    def get(self,request,id,format=None):
+        order = Order.objects.get(id=id)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+    def put(self,request,id,format=None):
+        order = Order.objects.get(id=id)
+        serializer = OrderSerializer(order,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,request,id,format=None):
+        order = Order.objects.get(id=id)
+        order.delete()
+        return Response("cart deleted", status=status.HTTP_204_NO_CONTENT)    
+              
+          
+    
+    
+  
